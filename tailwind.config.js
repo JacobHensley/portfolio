4f04734db8fac728689677dbf9b33f78edb5808f
JacobHensley <jacobdlhensley@gmail.com>
2024-05-06 19:35:45 -0500

    finished early testing draft

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,14 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    colors: {
+      'transparent': 'transparent',
+      'text': '#dce2f1',
+      'background': '#05070c',
+      'primary': '#93a3d4',
+      'secondary': '#522d70',
+      'accent': '#ac55bb',
+    },
     extend: {},
   },
   plugins: [],
